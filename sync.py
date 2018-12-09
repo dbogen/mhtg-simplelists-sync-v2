4b0c2ca1ad98e80542ff0b128e1c18b709d5e347
David Bogen <djb@bogen.org>
2018-12-08 21:16:03 -0600

    Switch to storing Simplelists ID along with e-mail in a dict; add comments.

--- sync.py
+++ sync.py
@@ -18,24 +18,30 @@ def kaput(error_string):
     
 def read_config(config_file):
     if exists(config_file):
-        config_fh = open(config_file)
-        config = json.load(config_fh)
+        config_fh = open(config_file) # create filehandle
+        config = json.load(config_fh) # read file json into dict
         config_fh.close
         return(config)
     else:
         kaput("Config file not found.")
 
 def get_oauth_token(config):
-    client_id = config['client_id'] 
-    client_secret = config['client_secret']
-    simplelists = OAuth2Session(client=LegacyApplicationClient(client_id=client_id))
+    #
+    # Fetch an OAUTH token from simplelists and stick it into a Requests
+    # object that we can use to query the Simplelists API.
+    #
+    simplelists = OAuth2Session(client=LegacyApplicationClient(client_id=config['client_id']))
     token = simplelists.fetch_token(token_url=config['token_url'],
         username=config['username'], password=config['password'], 
-        client_id=client_id, client_secret = client_secret)
+        client_id=config['client_id'], client_secret=config['client_secret'])
     return(simplelists)
 
 def query_api(api,config):
-    returned_addr = []
+    current_subscribers = {}
+    #
+    # Create a dict that we can turn into a json object to query the
+    # simplelists API.  This is their specified format.
+    #
     query_json = {'entity':'contact',
             'action': 'get',
             'params': {
@@ -49,14 +55,39 @@ def query_api(api,config):
                     ]
                 }}
     json_obj = json.dumps(query_json)
+    #
+    # Query the API using the Results object, the OAUTH token, and the
+    # JSON object we created above.
+    #
     result = api.post(config['api_url'],data=json_obj)
-    if result.status_code == 200:
+    if result.status_code == 200: # keep going only if we had success
+        #
+        # The API supplies results in JSON.
+        # Load the results into a dict so that we can iterate over them.
+        #
         result_dict = json.loads(result.text)
+        #
+        # there's probably a better way to do this, but this is what I
+        # came up with right now.
+        #
         for id_num in result_dict['return']:
             for e_mail_obj in range(0,len(result_dict['return'][id_num]['emails'])):
                 raw_returned_addr = str(result_dict['return'][id_num]['emails'][e_mail_obj]['email'])
-                returned_addr.append(raw_returned_addr.lower())
-        return(returned_addr)
+                #
+                # Store the e-mail as the dict key; store the Simplelists
+                # ID numbers as the value.  We need both for later
+                # operations.
+                #
+                current_subscribers[raw_returned_addr.lower()] = id_num
+        #
+        # Simple circuit breaker here.  If we get fewer than 350 entries
+        # in the dict, abort.  Something is probably wrong.
+        #
+        if len(current_subscribers) < 350:
+            kaput("We didn't get as many subscribers as we should have.  Exiting to prevent disaster.")
+        else:
+            pass
+        return(current_subscribers)
 
         
     else:
@@ -64,6 +95,11 @@ def query_api(api,config):
 
 def get_club_members(config):
     club_members = []
+    #
+    # This try/except block is pretty much right out of the
+    # mysql.connector docs.  Connect to the database and create a 
+    # connection object.
+    #
     try:
         cnx = mysql.connector.connect(
               user=config['db_username'],
@@ -78,21 +114,54 @@ def get_club_members(config):
         else:
           kaput(err)
     else:
+        #
+        # We're in.  create a cursor to conduct queries for us.
+        # Pack in the canned query in the config.json
+        #
         cursor = cnx.cursor()
         cursor.execute(config['db_query_string'])
+        #
+        # We get back a bunch of fields we don't need; we only need
+        # email so throw away everything else.
+        #
         for (ignored1, email, ignored2, ignored3, ignored4) in cursor:
             raw_email = str(email)
+            #
+            # Turn e-mail to lowercase (like we did above for what we got
+            # out of the API) so that comparisons between the two are 
+            # easier.
+            #
             club_members.append(raw_email.lower())
         cursor.close()
-        cnx.close()
-        return(club_members)
+    cnx.close()
+    #
+    # Again, another simple circuit breaker.  If we got fewer than 350
+    # entries from the database, abort.
+    #
+    if len(club_members) < 350:
+        kaput("We got fewer than 350 club members from the database. Exiting to prevent disaster.")
+    else:
+        pass
+    return(club_members)
 
 def compare_lists(club_members,current_subscribers):
     members_to_add = []
 
+    #
+    # The goal here is to return two things:
+    # 1) A list of e-mail address in the club db that need to be added to
+    #    the simplelists address book and the members list.
+    # 2) A list of the e-mail addresses in the simplelists contact list
+    #    that needed to be removed because they are no longer club members.
+    #
+    # Do that by removing all e-mail addresses between the two lists that
+    # are the same.  That leaves people who need to come off the list
+    # on the Simpleilsts roster. Create a list of people who need to be
+    # added to Simpleilsts.
+    #
     for club_member in club_members:
         if club_member in current_subscribers:
-            current_subscribers.remove(club_member)
+            del current_subscribers[club_member]
         else:
             members_to_add.append(club_member)
     return(current_subscribers,members_to_add)
